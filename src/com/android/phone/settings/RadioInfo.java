734dab0e101b73dae107f4bea76f3059c3c96df0
Ido Ben-Hur <idoybh2@gmail.com>
2024-10-03 20:46:36 +0300

    RadioInfo: Handle system insets
    A15 now enforces edge-to-edge on all of the apps that target it.
    Adapt.
    
    Change-Id: Iac9e50b9893cf93d9305da863780c951fa702886
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/phone/settings/RadioInfo.java
+++ src/com/android/phone/settings/RadioInfo.java
@@ -21,6 +21,7 @@ import static android.net.ConnectivityManager.NetworkCallback;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 import android.annotation.NonNull;
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -92,6 +93,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowInsetsController;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -104,6 +106,10 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AlertDialog.Builder;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.graphics.Insets;
 
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneFactory;
@@ -579,6 +585,19 @@ public class RadioInfo extends CollapsingToolbarBaseActivity {
         mActionEsosDemo =
             r.getString(
                     com.android.internal.R.string.config_satellite_demo_mode_sos_intent_action);
+
+        // Handle window insets for padding adjustments
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main_layout), (view, insets) -> {
+            Insets systemInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            view.setPadding(
+                view.getPaddingLeft(),
+                systemInsets.top,
+                view.getPaddingRight(),
+                systemInsets.bottom
+            );
+            return insets;
+        });
 
         mQueuedWork = new ThreadPoolExecutor(1, 1, RUNNABLE_TIMEOUT_MS, TimeUnit.MICROSECONDS,
                 new LinkedBlockingDeque<>());
