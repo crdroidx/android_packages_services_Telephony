84e00759c41408921169760f2ce00d8fe3c867dd
Cole Faust <colefaust@google.com>
2025-01-27 10:02:13 -0800

    Fix errorprone warnings
    https://errorprone.info/bugpattern/BadInstanceof
    https://errorprone.info/bugpattern/CompareToZero
    
    Bug: 253827323
    Test: Presubmits
    Flag: EXEMPT refactor
    Google: 3466604
    Change-Id: I19bf4cc3a82e6cdc9d09cd23e1213b07139403ea

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -2339,9 +2339,8 @@ public class TelephonyConnectionService extends ConnectionService {
                                 }
                             }
                             for (Conference c : getAllConferences()) {
-                                if (c.getState() != Connection.STATE_DISCONNECTED
-                                        && c instanceof Conference) {
-                                    ((Conference) c).onDisconnect();
+                                if (c.getState() != Connection.STATE_DISCONNECTED) {
+                                    c.onDisconnect();
                                 }
                             }
                         } else if (!isVideoCallHoldAllowed(phone)) {
